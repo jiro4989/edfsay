21d0d9f1e136170ca1031455e2fc91159dd8430b
jiro4989 <jiroron666@gmail.com>
2019-05-28 00:25:06 +0900

    :construction: 重戦車の力、見せてやるぜぇ！！

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 
 set -eux
 
-sudo cp -p bin/edfsay /usr/local/bin/
+readonly DIR_PREFIX=/usr/local
+readonly BIN_NAME=edfsay
+
+sudo install -m 0755 "bin/$BIN_NAME" "$DIR_PREFIX/bin/"
+sudo install -d -m 0755 "$DIR_PREFIX/etc/$BIN_NAME"
+sudo install -m 0644 "etc/$BIN_NAME"/* "$DIR_PREFIX/etc/$BIN_NAME"
