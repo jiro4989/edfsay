a4f29ccbafdba12ee34b4765e66552ed94e11a4f
jiro4989 <jiroron666@gmail.com>
2019-05-28 11:21:27 +0900

    :construction: つまり、殴れということだ

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ set -eux
 readonly DIR_PREFIX=/usr/local
 readonly BIN_NAME=edfsay
 
-sudo install -m 0755 "bin/$BIN_NAME" "$DIR_PREFIX/bin/"
+sudo install -m 0755 "bin/$BIN_NAME"* "$DIR_PREFIX/bin/"
 sudo install -d -m 0755 "$DIR_PREFIX/etc/$BIN_NAME"
 sudo install -m 0644 "etc/$BIN_NAME"/* "$DIR_PREFIX/etc/$BIN_NAME"
