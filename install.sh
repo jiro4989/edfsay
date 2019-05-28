4ab9f2aa1ddaab1b475bd65c5cd5afa72a19845f
jiro4989 <jiroron666@gmail.com>
2019-05-28 17:24:10 +0900

    :construction: 砲撃は苛烈！繰り返す！砲撃は苛烈！

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 set -eux
 
 readonly DIR_PREFIX=/usr/local
-readonly BIN_NAME=edfsay
+readonly BIN_NAME=edf
+readonly PROJECT_NAME=edfsay
 
 sudo install -m 0755 "bin/$BIN_NAME"* "$DIR_PREFIX/bin/"
-sudo install -d -m 0755 "$DIR_PREFIX/etc/$BIN_NAME"
-sudo install -m 0644 "etc/$BIN_NAME"/* "$DIR_PREFIX/etc/$BIN_NAME"
+sudo install -d -m 0755 "$DIR_PREFIX/etc/$PROJECT_NAME"
+sudo install -m 0644 "etc/$PROJECT_NAME"/* "$DIR_PREFIX/etc/$PROJECT_NAME"
