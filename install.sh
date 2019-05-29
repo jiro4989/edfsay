23817acd0cb6e9f55f78ff4da486098d7ee6453b
jiro4989 <jiroron666@gmail.com>
2019-05-29 11:55:48 +0900

    :memo: ウイングダイバーは巨大生物の天敵！到着すれば、巨大生物もお終いだ！

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ readonly DIR_PREFIX=/usr/local
 readonly BIN_NAME=edf
 readonly PROJECT_NAME=edfsay
 
-sudo install -m 0755 "bin/$BIN_NAME"* "$DIR_PREFIX/bin/"
-sudo install -d -m 0755 "$DIR_PREFIX/etc/$PROJECT_NAME"
-sudo install -m 0644 "etc/$PROJECT_NAME"/* "$DIR_PREFIX/etc/$PROJECT_NAME"
+install -m 0755 "bin/$BIN_NAME"* "$DIR_PREFIX/bin/"
+install -d -m 0755 "$DIR_PREFIX/etc/$PROJECT_NAME"
+install -m 0644 "etc/$PROJECT_NAME"/* "$DIR_PREFIX/etc/$PROJECT_NAME"
